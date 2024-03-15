26d3348aab233f874ba889e0bc3292bcebeda394
jadidbourbaki <tprod4242@gmail.com>
2024-03-15 11:58:40 -0400

    Added LCM

--- gcd.go
+++ gcd.go
@@ -12,3 +12,20 @@ func Gcd(a, b *big.Int) *big.Int {
 
 	return Gcd(b, new(big.Int).Mod(a, b))
 }
+
+func Lcm(a, b *big.Int) *big.Int {
+
+	gcd := Gcd(a, b)
+
+	zero := new(big.Int)
+	zero.SetInt64(0)
+
+	if gcd.Cmp(zero) == 0 {
+		return zero
+	}
+
+	lcm := new(big.Int)
+	lcm.Mul(a, b)
+	lcm.Div(lcm, gcd)
+	return lcm
+}
--- gcd_test.go
+++ gcd_test.go
@@ -21,3 +21,18 @@ func TestGcdTrivial(t *testing.T) {
 
 	assert.Equal(t, mygcd, zero)
 }
+
+func TestLcmTrivial(t *testing.T) {
+	a := new(big.Int)
+	a.SetInt64(0)
+
+	b := new(big.Int)
+	b.SetInt64(0)
+
+	mylcm := Lcm(a, b)
+
+	zero := new(big.Int)
+	zero.SetInt64(0)
+
+	assert.Equal(t, mylcm, zero)
+}
