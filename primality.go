c61ebf310f6af0934216e26f833d83347d436c9f
jadidbourbaki <tprod4242@gmail.com>
2024-03-16 20:27:54 -0400

    Updated Fermat test

--- primality.go
+++ primality.go
@@ -6,34 +6,34 @@ import (
 )
 
 func FermatTest(n *big.Int, k uint) bool {
-	if n.Cmp(new(big.Int).SetInt64(0)) == -1 {
+	if n.Cmp(big.NewInt(0)) == -1 {
 		panic("cannot be negative")
 	}
 
-	if n.Cmp(new(big.Int).SetInt64(1)) == 0 ||
-		n.Cmp(new(big.Int).SetInt64(4)) == 0 {
+	if n.Cmp(big.NewInt(1)) == 0 ||
+		n.Cmp(big.NewInt(4)) == 0 {
 		return false
 	}
 
-	if n.Cmp(new(big.Int).SetInt64(2)) == 0 ||
-		n.Cmp(new(big.Int).SetInt64(3)) == 0 {
+	if n.Cmp(big.NewInt(2)) == 0 ||
+		n.Cmp(big.NewInt(3)) == 0 {
 		return true
 	}
 
 	for i := uint(0); i < k; i++ {
 		nMinusFour := new(big.Int)
-		nMinusFour.Sub(n, new(big.Int).SetInt64(4))
+		nMinusFour.Sub(n, big.NewInt(4))
 
 		a, _ := rand.Int(rand.Reader, nMinusFour)
-		a.Add(a, new(big.Int).SetInt64(2))
+		a.Add(a, big.NewInt(2))
 
 		nMinusOne := new(big.Int)
-		nMinusOne.Sub(n, new(big.Int).SetInt64(1))
+		nMinusOne.Sub(n, big.NewInt(1))
 
 		result := new(big.Int)
 		result.Exp(a, nMinusOne, n)
 
-		if result.Cmp(new(big.Int).SetInt64(1)) != 0 {
+		if result.Cmp(big.NewInt(1)) != 0 {
 			return false
 		}
 
